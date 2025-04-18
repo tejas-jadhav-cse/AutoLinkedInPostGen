8e142a34fe5cf1fb6fea71817a8623ceeeb955df
tejas-jadhav-cse <tejas.jadhav.cse@gmail.com>
2025-04-18 22:17:11 +0530

    setup src folder

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AutoLinkedInPostGen from "./AutoLinkedInPostGen";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-950 text-white p-6">
+      <AutoLinkedInPostGen />
+    </div>
+  );
+}
+
+export default App;
